9f1c81560e16c61d1c9d05ef1f3f386b7469bb3b
Anthony Pogliano <poglianoster@gmail.com>
2022-04-21 11:16:08 -0600

    Updating function.

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,6 @@
-exports.handler = async function (event, context) {
-  return {
+exports.handler = async function (event, context, callback) {
+  callback(null, {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello World' }),
-  }
+  })
 }
