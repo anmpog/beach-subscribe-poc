41347ec04abf8882d31afe6ceca281cba6db14cf
Anthony Pogliano <poglianoster@gmail.com>
2022-04-21 14:05:09 -0600

    Setup function to get data from a 3rd party API.

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,16 +1,17 @@
-import fetch from 'node-fetch'
+const axios = require('axios')
 
-exports.handler = async function (event, context, callback) {
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then((res) => {
+exports.handler = function (event, context, callback) {
+  axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => {
       callback(null, {
         statusCode: 200,
         body: res.data.title,
       })
     })
-    .catch((error) => {
-      callback((error) => {
-        console.log('There was an error fetching the data: ', error)
+    .catch(error => {
+      callback(error, {
+        statusCode: 418,
+        body: JSON.stringify("There was an error requesting data.")
       })
     })
 }
