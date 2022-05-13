9d6ab1e4af754740c91ca2409d1410350aebf901
Anthony Pogliano <poglianoster@gmail.com>
2022-05-12 18:20:21 -0700

    Minor changes.

--- functions/getAccessToken.js
+++ functions/getAccessToken.js
@@ -1,5 +1,4 @@
 const axios = require('axios')
-const token = process.env.TWITTER_API_BEARER_TOKEN
 
 exports.handler = async function (event, context, callback) {
   // Step 3 - POST oauth/access_token
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+console.log('Gatsby Env Details: ', process.env.NODE_ENV)
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
